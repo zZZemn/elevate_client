975ba42ac46a09d7d2fda71461f5847ab72a4792
Emmanuel Ugaban <>
2024-06-28 18:17:15 +0800

    images design

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -36,15 +36,75 @@ function LandingPage() {
       ) : (
         <div className="container mx-auto p-4">
           <h1 className="text-2xl font-bold mb-4">Posts</h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
             {dataPosts.map((post) => (
-              <div key={post._id} className="bg-white p-4 rounded shadow">
-                <img
-                  src={post.postedBy.picture}
-                  alt={`${post.postedBy.firstName}'s profile`}
-                  className="w-16 h-16 rounded-full"
-                />
-                <h2 className="text-xl font-semibold">{post.caption}</h2>
+              <div key={post._id} className="bg-white p-2 rounded shadow w-4/5">
+                <div className="flex items-center">
+                  <img
+                    src={post.postedBy.picture}
+                    alt={`${post.postedBy.firstName}'s profile`}
+                    className="w-5 h-5 rounded-full"
+                  />
+                  <p className="text-sm font-medium mx-3">
+                    {post.postedBy.firstName + " " + post.postedBy.lastName}
+                  </p>
+                </div>
+                <div className="mt-4 bg-black flex justify-center">
+                  <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXOmr3zAEaBLWe9jQbBVgDAMORmnpRzHO-tQ&s"
+                    alt="sample"
+                  />
+                </div>
+                <div className="flex mt-2">
+                  <button className="mx-1">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                      />
+                    </svg>
+                  </button>
+                  <button className="mx-1">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z"
+                      />
+                    </svg>
+                  </button>
+                  <button className="mx-1">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z"
+                      />
+                    </svg>
+                  </button>
+                </div>
               </div>
             ))}
           </div>
