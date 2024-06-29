7e6c2529975d251478661b977d083526745d8f98
Emmanuel Ugaban <>
2024-06-29 19:51:01 +0800

    Sign up page

---
+++ src/components/formSelect.jsx
@@ -0,0 +1,28 @@
+function FormSelect({ label, professions }) {
+  return (
+    <>
+      <div className="relative w-full max-w-sm mt-3">
+        <label
+          className="absolute font-extrabold p-1 px-2"
+          style={{ fontSize: "10px" }}
+        >
+          {label}
+        </label>
+        <select
+          name=""
+          id=""
+          className="w-full max-w-sm border border-gray-900 rounded-lg p-2 pt-4 bg-white text-gray-700 placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent"
+          style={{ fontSize: "14px" }}
+        >
+          <option>Please select profession</option>
+          {professions.map((profession) => (
+            <option value={profession.profession} key={profession._id}>
+              {profession.profession}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
+  );
+}
+export default FormSelect;
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,12 +1,57 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import LandingNavBar from "../components/LandingNavBar";
+import Loading from "../components/Loading";
+import FormBtnSubmit from "../components/formBtnSubmit";
+import FormSelect from "../components/formSelect";
+import FormTextField from "../components/formTextField";
 
 function Signup() {
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const [professions, setProfessions] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(apiUrl + "/profession")
+      .then((response) => {
+        setProfessions(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (error) {
+    return <center>Error: {error.message}</center>;
+  }
+
+  console.log(professions);
+
   return (
     <>
-      <div>
-        <LandingNavBar />
-        <h1>Signup</h1>
-      </div>
+      <LandingNavBar />
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="bg-login flex justify-center items-center">
+          <div className="bg-login-form  w-full max-w-sm p-3 py-10 mb-40 rounded-lg shadow-md">
+            <center className="font-extrabold text-xl">SIGN UP</center>
+            <div className="mt-10">
+              <FormTextField type={"text"} label={"First Name"} />
+              <FormTextField type={"text"} label={"Last Name"} />
+              <FormSelect label={"Profession"} professions={professions} />
+              <FormTextField type={"email"} label={"Email"} />
+              <FormTextField type={"text"} label={"Username"} />
+              <FormTextField type={"password"} label={"Password"} />
+              <FormBtnSubmit label={"Sign up"} />
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
