091d368e178021f109a23ffe4d78cc5cf1b82e4c
Emmanuel Ugaban <>
2024-07-04 20:38:27 +0800

    Client home page

--- src/pages/Client/HomePage.jsx
+++ src/pages/Client/HomePage.jsx
@@ -2,27 +2,37 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import NavBar from "./components/NavBar";
 import SideBar from "./components/SideBar";
+import Post from "../../components/Post";
+import Loading from "../../components/Loading";
 
 function HomePage() {
   const apiUrl = import.meta.env.VITE_API_URL;
-  const [userData, setuserData] = useState(null);
+  const [userData, setUserData] = useState(null);
+  const [dataPosts, setDataPosts] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const ctoken = sessionStorage.getItem("ctoken");
   const [showSideBar, setShowSideBar] = useState(false);
 
   useEffect(() => {
-    axios
-      .get(apiUrl + "/user/" + ctoken)
-      .then((response) => {
-        setuserData(response.data);
+    const fetchData = async () => {
+      try {
+        const [userResponse, postResponse] = await Promise.all([
+          axios.get(apiUrl + "/user/" + ctoken),
+          axios.get(apiUrl + "/post"),
+        ]);
+
+        setUserData(userResponse.data);
+        setDataPosts(postResponse.data);
         setLoading(false);
-      })
-      .catch((error) => {
+      } catch (error) {
         setError(error);
         setLoading(false);
-      });
-  }, []);
+      }
+    };
+
+    fetchData();
+  }, [apiUrl, ctoken]);
 
   const handleLogout = () => {
     sessionStorage.removeItem("ctoken");
@@ -39,11 +49,17 @@ function HomePage() {
 
   return (
     <>
-      <h1>
-        <NavBar handleSideBar={handleSideBar} />
-        {showSideBar && <SideBar handleLogout={handleLogout} />}
-        <center>Client</center>
-      </h1>
+      <NavBar handleSideBar={handleSideBar} />
+      {showSideBar && <SideBar handleLogout={handleLogout} />}
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+          {dataPosts.map((post) => (
+            <Post key={post._id} post={post} />
+          ))}
+        </div>
+      )}
     </>
   );
 }
