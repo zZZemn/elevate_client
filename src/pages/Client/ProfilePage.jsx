9795a83cfd5b120515db5fa5de4dd1be88482734
Emmanuel Ugaban <>
2024-07-25 16:05:28 +0800

    remove unused codes

--- src/pages/Client/ProfilePage.jsx
+++ src/pages/Client/ProfilePage.jsx
@@ -2,43 +2,16 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import NavBar from "./components/NavBar";
 import SideBar from "./components/SideBar";
-import Post from "../../components/Post";
 import Loading from "../../components/Loading";
-import PostButton from "./components/PostButton";
-import PostModal from "./components/PostModal";
-import Comments from "./components/Comments";
 
 function ProfilePage() {
   const apiUrl = import.meta.env.VITE_API_URL;
   const ctoken = sessionStorage.getItem("ctoken");
   const [userData, setUserData] = useState(null);
   const [dataPosts, setDataPosts] = useState([]);
-  const [likes, setLikes] = useState({});
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [showSideBar, setShowSideBar] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-
-  // Comments
-  const [showComments, setShowComments] = useState(false);
-  const [postViewComments, setPostViewComments] = useState({
-    postedBy: "",
-  });
-
-  const fetchLikes = async (posts) => {
-    if (!posts || posts.length === 0) {
-      return;
-    }
-
-    const likesMap = {};
-    for (const post of posts) {
-      const isLiked = await checkReaction(post._id);
-      likesMap[post._id] = isLiked;
-    }
-    setLikes(likesMap);
-
-    console.log("Fetch function");
-  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -60,10 +33,6 @@ function ProfilePage() {
     fetchData();
   }, [apiUrl, ctoken]);
 
-  useEffect(() => {
-    fetchLikes(dataPosts);
-  }, [dataPosts]);
-
   const handleLogout = () => {
     sessionStorage.removeItem("ctoken");
     console.log("Token removed");
@@ -73,47 +42,6 @@ function ProfilePage() {
   const handleSideBar = () => {
     setShowSideBar(!showSideBar);
     console.log(showSideBar);
-  };
-
-  const handleShowModal = (val) => {
-    setShowModal(val);
-    setShowComments(val);
-  };
-
-  const handleShowComment = (post) => {
-    setShowComments(true);
-    setPostViewComments(post);
-  };
-
-  const checkReaction = async (postId) => {
-    try {
-      const response = await axios.get(
-        apiUrl + "/react/" + postId + "/" + userData._id
-      );
-      console.log(response);
-      return response.data.reacted;
-    } catch (err) {
-      console.error(err);
-      return false;
-    }
-  };
-
-  const handleReaction = (postId, reactionType) => {
-    const reaction = {
-      userId: userData._id,
-      postId: postId,
-      reaction: reactionType,
-    };
-
-    console.log(reaction);
-
-    axios
-      .post(apiUrl + "/react", reaction)
-      .then((response) => {
-        console.log(response);
-        fetchLikes(dataPosts);
-      })
-      .catch((err) => console.error(err));
   };
 
   return (
